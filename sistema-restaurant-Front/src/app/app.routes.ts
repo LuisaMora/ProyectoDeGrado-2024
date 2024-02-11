5d3b02156bd3c4670b709684824622500c4c7449
Luisa Mora <luisamora895@gmail.com>
2024-02-11 19:21:05 -0400

    creando componentes registrar y editar

--- sistema-restaurant-Front/src/app/app.routes.ts
+++ sistema-restaurant-Front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RegistrarPlatilloComponent } from './modulos/menu/registrar-platillo/registrar-platillo.component';
+import { EditarPlatilloComponent } from './modulos/menu/editar-platillo/editar-platillo.component';
+//importar el modulo creado
+export const routes: Routes = [
+    {
+        path:'registrar-platillo', 
+        component: RegistrarPlatilloComponent
+    },
+    {
+        path:'editar-platillo', 
+        component: EditarPlatilloComponent
+    }
+];
