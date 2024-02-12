da1596b2a258c7a26b34f369085ef7849a62a815
Luisa Mora <luisamora895@gmail.com>
2024-02-12 11:22:46 -0400

    add componentes

--- sistema-restaurant-Front/src/app/app.routes.ts
+++ sistema-restaurant-Front/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RegistrarComponent } from './modulos/menu/registrar/registrar.component';
+import { ListaComponent } from './modulos/menu/lista/lista.component';
+export const routes: Routes = [
+    {
+        path:'registrar-platillo',
+        component:RegistrarComponent
+    },
+    {
+        path:'lista-platillos',
+        component:ListaComponent
+    } 
+];
