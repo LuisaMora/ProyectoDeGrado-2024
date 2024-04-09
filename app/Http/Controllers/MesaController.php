1556e9e5ee18ae4a705405b87bcee97c46a6e748
Gomer Claure <gomerclaureb@gmail.com>
2024-04-09 11:42:18 -0400

    ADD mostrando lista de mesas para empleados

--- app/Http/Controllers/MesaController.php
+++ app/Http/Controllers/MesaController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Empleado;
 use App\Models\Propietario;
+use App\Models\User;
 
 class MesaController extends Controller
 {
     public function index()
     {
         $id_usuario = auth()->user()->id;
-        $propietario = Propietario::where('id_usuario', $id_usuario)->first();
-        if ($propietario) {
-            $mesas = $propietario->restaurante->mesa;
-        
-            return response()->json(['status' => 'success', 'mesas' => $mesas], 200);
+        $usuario = User::find($id_usuario);
+        $tipo_usuario = $usuario->getTipoUsuario();
+
+        if($tipo_usuario == 'Propietario'){
+            $mesas = Propietario::where('id_usuario', $id_usuario)->first()->restaurante->mesa;
+        }elseif($tipo_usuario == 'Empleado') {
+            $mesas = Empleado::where('id_usuario', $id_usuario)->first()->propietario->restaurante->mesa;
         }else{
-            return response()->json(['status' => 'error', 'message' => 'No se encontro al propietario'], 404);
+            return response()->json(['status' => 'error', 'message' => 'Este usuario no puede ver las mesas'], 403);
         }
+        return response()->json(['status' => 'success', 'mesas' => $mesas], 200);
+        
 
     }
 }
