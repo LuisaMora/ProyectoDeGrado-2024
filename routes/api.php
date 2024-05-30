77cd393dcb67e906449e60ce8f5aead508f00003
Luisa Mora <79343096+LuisaMora@users.noreply.github.com>
2024-05-30 19:02:04 -0400

    Feature/mostrar pedidos (#12)
    * Comenzando con estategia manejo de ramas git flow
    
    * uniendo back
    
    * Agregando cambios
    
    * FIX bug eliminarCategoria
    
    * UPDATE archivos que ignorar
    
    * DELETE archivos de seguimiento
    
    * Fix quitando .env
    
    * FIX quitando vendor de los archivos del backend
    
    * FIX refactor auth y restringiendo lo subido a github
    
    * ADD cambios para refactorizar el codigo
    
    * FIX muestra de platillos  control de vacios
    
    * FIX error de editar categoria y que no se borre la imagen
    
    * FIX error al eliminar categoria
    
    * Obteniendo tablas de pedidos
    
    ---------
    
    Co-authored-by: Gomer Claure <gomerclaureb@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/prueba_admin', function () {
             return response()->json(['message' => 'Bienvenido administrador']);
         });
-    });
+    }); 
     
     Route::middleware('empleado')->group(function () {
         Route::get('/menu/pedido', 'App\Http\Controllers\PlatilloController@index');
@@ -51,6 +51,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/prueba_empleado', function () {
             return response()->json(['message' => 'Bienvenido empleado','auth' => auth()->user()]);
         });
+        Route::get('/pedidos', 'App\Http\Controllers\PedidoController@index');
     });
 
     Route::middleware('propietarioOempleado')->group(function () {
