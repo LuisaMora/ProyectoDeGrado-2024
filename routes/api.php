fda3d450f5809f3d96073233baad0978169dde4f
Luisa Mora <79343096+LuisaMora@users.noreply.github.com>
2024-10-07 09:31:31 -0400

    Mostrar lista de cuentas cerradas (#27)

--- routes/api.php
+++ routes/api.php
@@ -109,6 +109,9 @@ Route::middleware('auth:sanctum')->group(function () {
         
         Route::post('/cuenta/store/{idRestaurante}', 'App\Http\Controllers\Pedido\CuentaController@store');
         Route::post('/cuenta/close/{id}', 'App\Http\Controllers\Pedido\CuentaController@close');
+
+        Route::get('/cuentas/cerradas', 'App\Http\Controllers\Pedido\CuentaController@showCerradas');
+
     });
 
     Route::middleware('empleado:cajero')->group(function () {
